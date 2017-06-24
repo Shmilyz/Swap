d2c493e33c1797f9b0568c247e60bd531357022b
Your Name <zunzunzun8@163.com>
2017-06-24 23:44:09 +0800

    增加添加用户地址，填写用户地址。

--- app/src/main/java/com/shmily/tjz/swap/ShowPositionsActivity.java
+++ app/src/main/java/com/shmily/tjz/swap/ShowPositionsActivity.java
@@ -1,15 +1,13 @@
 package com.shmily.tjz.swap;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
-public class BuyActivity extends AppCompatActivity {
+public class ShowPositionsActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buy);
-
+        setContentView(R.layout.activity_show_positions);
     }
 }
